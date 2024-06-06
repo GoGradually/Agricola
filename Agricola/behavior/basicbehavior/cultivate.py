6910a6acd796f37d6cacf9b452f293d6d7668cdb
trueu1120 <trueu1120@gmail.com>
2024-06-06 22:39:44 +0900

    보조 설비
    주석 변경
    
    주석 변경

--- Agricola/behavior/basicbehavior/cultivate.py
+++ Agricola/behavior/basicbehavior/cultivate.py
@@ -13,13 +13,13 @@ from repository.player_status_repository import player_status_repository
 from repository.round_status_repository import round_status_repository
 from behavior.basebehavior.arable_expansion import ArableExpansion
 
-
-# Todo
-
-class Cultivate(BehaviorInterface):
+class Cultivate(Command):
     def __init__(self, field_status):
         self.log_text = ""
         self.field_status = field_status
+
+    def can_play(self):
+        return True
 
     def execute(self):
         doArable = ArableExpansion(self.field_status)
@@ -29,6 +29,7 @@ class Cultivate(BehaviorInterface):
         else:
             self.log_text = doArable.log()
             return False
+
 
     def log(self):
         return self.log_text
