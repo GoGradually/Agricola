c56710a1112500df1bb54ae63114015282ed5a6f
GoGradually <chkun3109@gmail.com>
2024-06-08 09:55:29 +0900

    버그 수정

--- Agricola/behavior/roundbehavior/seed_bake.py
+++ Agricola/behavior/roundbehavior/seed_bake.py
@@ -9,13 +9,8 @@
 from behavior.basebehavior.do_bake import DoBake
 from behavior.behavior_interface import BehaviorInterface
 from behavior.unitbehavior.use_worker import UseWorker
-from command import Command
-from copy import copy
-from entity.crop_type import CropType
-from entity.round_behavior_type import RoundBehaviorType
 from repository.game_status_repository import game_status_repository
 from repository.player_status_repository import player_status_repository
-from repository.round_status_repository import round_status_repository
 from behavior.basebehavior.seed_plant import SeedPlant
 
 
